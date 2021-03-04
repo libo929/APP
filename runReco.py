3ccb5ad8039a1040b2080324b9ee3ffce7b229a1
libo929 <libo929@163.com>
2021-03-04 18:21:25 +0800

    Added runReco.py

---
+++ runReco.py
@@ -0,0 +1,11 @@
+import os
+
+for energy in range(10, 90, 10):
+    rootfile = '/home/libo/workplace/CaloSim/script/output/gamma_' + str(energy) + 'GeV/caloHits.root'
+    os.system('ln -sf ' + rootfile + ' caloHits.root')
+
+    recofile = 'reco_gamma_' + str(energy) + 'GeV.root'
+    os.system('./bin/runRec -i settings/test.xml -n 3000 > /dev/null')
+
+    os.system('mv -f test.root ' + recofile)
+    print(str(energy) + ' GeV done.')
