bc4516d2a57f9a0f41d5eac22de4f133a9cf0c13
libo929 <libo929@163.com>
2021-03-07 16:19:59 +0800

    Updated the runReco script

--- runReco.py
+++ runReco.py
@@ -1,11 +1,14 @@
 import os
 
-for energy in range(10, 90, 10):
-    rootfile = '~/workplace/CaloSim/script/output/gamma_' + str(energy) + 'GeV/caloHits.root'
+particle = 'gamma'
+maxEnergy = 90
+
+for energy in range(10, maxEnergy, 10):
+    rootfile = '~/workplace/CaloSim/script/output/' + particle + '_' + str(energy) + 'GeV/caloHits.root'
     os.system('ln -sf ' + rootfile + ' caloHits.root')
 
-    recofile = 'reco_gamma_' + str(energy) + 'GeV.root'
-    os.system('./bin/runRec -i settings/test.xml -n 3000 > /dev/null')
+    recofile = 'reco_' + particle + '_' + str(energy) + 'GeV.root'
+    os.system('./bin/runRec -i settings/test.xml -n 10000 > /dev/null')
 
     os.system('mv -f test.root ' + recofile)
     print(str(energy) + ' GeV done.')
